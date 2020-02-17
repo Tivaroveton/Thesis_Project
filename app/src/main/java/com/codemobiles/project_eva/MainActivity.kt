863a33e2ab1d2afe9d1c523857736d6493f3af21
Tivaroveton <23227274+Tivaroveton@users.noreply.github.com>
2020-02-17 12:17:38 +0700

    first commit

---
+++ app/src/main/java/com/codemobiles/project_eva/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.codemobiles.project_eva
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun onClickLogin(view: View) {
+        val intent = Intent(this, FeedActivity::class.java)
+        startActivity(intent)
+    }
+
+
+
+}
